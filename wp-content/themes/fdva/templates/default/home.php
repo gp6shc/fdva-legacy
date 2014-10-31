bd196175684f7d0770e1dbcea450eeb9556f6d3e
Brian <briano@sachsmedia.com>
2014-10-31 10:50:59 -0400

    menu / setup

--- wp-content/themes/fdva/templates/default/home.php
+++ wp-content/themes/fdva/templates/default/home.php
@@ -10,17 +10,17 @@
 	<hr class="span12 middle">
 	<?php echo do_shortcode("[posts type=page show=3 loop=home-box tax_query=box/home offset=3 meta_key=order-home orderby=meta_value_num order=ASC]"); ?>
 	<div class="span12">
-		<hr>
+		<!--<hr>
 		<div class="thumb small align-left">
 		
-		<!--<img src="http://sachsdigital.com/test/fdva/wp-content/timthumb.php?src=http://sachsdigital.com/test/fdva/wp-content/uploads/2012/08/foundation.jpg" />-->
+		<img src="http://sachsdigital.com/test/fdva/wp-content/timthumb.php?src=http://sachsdigital.com/test/fdva/wp-content/uploads/2012/08/foundation.jpg" />
 		
 		
 		<?php pxl::timthumb( get_themeinfo('donate_img'), array( 'w' => 130, 'h' => 130 ), 'thumbnail' ); ?></div>
 		
 		
-		<h4>Florida Veterans Foundation</h4>
+		<h4>Florida Legacy of Service</h4>
 		<p><?php echo get_themeinfo('donate'); ?></p>
-		<a class="button" href="<?php themeinfo('donate_link'); ?>">Donate Today</a>
+		<a class="button" href="<?php themeinfo('donate_link'); ?>">Learn More</a>-->
 	</div>
 </div>
